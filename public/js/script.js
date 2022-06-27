68d539b5fae56ffaf1af8fe699df5eab1e6362c4
Leo <lyuholu@gmail.com>
2022-06-28 02:17:10 +0800

    add side bar
    it allows user to choose teams

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,30 @@
+function clickOn(league) {
+    document.getElementById('NBA').className = (league == 'NBA') ? 'active' : '';
+    document.getElementById('NFL').className = (league == 'NFL') ? 'active' : '';
+    document.getElementById('Bundesliga').className = (league == 'Bundesliga') ? 'active' : '';
+    document.getElementById('Premier League').className = (league == 'Premier League') ? 'active' : '';
+    renderPage(league);
+}
+
+function chooseThisTeam(i) {
+    document.getElementById(nbaTeams[i]['full_name']).classList.toggle('chosen');
+    chosenTeams.has(i) ? chosenTeams.delete(i) : chosenTeams.add(i);
+}
+
 function renderPage(league) {
     if (league == 'NBA') {
-
+        fetch('https://www.balldontlie.io/api/v1/teams')
+        .then(res => res.json())
+        .then((data) => {
+            nbaTeams = data.data;
+            console.log(nbaTeams);
+            let teams = document.getElementById('teams');
+            teams.innerHTML = '';
+            for (let i = 0; i < nbaTeams.length; i++) {
+                teams.innerHTML += `<div id="${nbaTeams[i]['full_name']}" class="team-card" onclick="chooseThisTeam(${i})">${nbaTeams[i]['full_name']}</div>`;
+            }
+            teams.innerHTML += '<br>';
+        });
     }
     else if (league == 'NFL') {
 
@@ -13,21 +37,10 @@ function renderPage(league) {
     }
 }
 
-function clickOn(league) {
-    document.getElementById('NBA').className = (league == 'NBA') ? 'active' : '';
-    document.getElementById('NFL').className = (league == 'NFL') ? 'active' : '';
-    document.getElementById('Bundesliga').className = (league == 'Bundesliga') ? 'active' : '';
-    document.getElementById('Premier League').className = (league == 'Premier League') ? 'active' : '';
-    renderPage(league);
-}
-
 let nbaTeams;
-
-fetch('https://www.balldontlie.io/api/v1/teams')
-.then(res => res.json())
-.then((data) => {
-    nbaTeams = data.data;
-    console.log(nbaTeams);
-});
+let nflTeams;
+let BundesligaTeams;
+let PremierLeagueTeams;
+const chosenTeams = new Set();
 
 renderPage('NBA');
