9b64f8d8ee9bf739745751f34ae2a90d120ed8e5
Leo <lyuholu@gmail.com>
2022-06-27 20:36:34 +0800

    nav bar

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,26 @@
+function renderPage(league) {
+    if (league == 'NBA') {
+
+    }
+    else if (league == 'NFL') {
+
+    }
+    else if (league == 'Bundesliga') {
+        
+    }
+    else if (league == 'Premier League') {
+        
+    }
+}
+
+function clickOn(league) {
+    document.getElementById('NBA').className = (league == 'NBA') ? 'active' : '';
+    document.getElementById('NFL').className = (league == 'NFL') ? 'active' : '';
+    document.getElementById('Bundesliga').className = (league == 'Bundesliga') ? 'active' : '';
+    document.getElementById('Premier League').className = (league == 'Premier League') ? 'active' : '';
+    renderPage(league);
+}
+
 let nbaTeams;
 
 fetch('https://www.balldontlie.io/api/v1/teams')
@@ -6,3 +29,5 @@ fetch('https://www.balldontlie.io/api/v1/teams')
     nbaTeams = data.data;
     console.log(nbaTeams);
 });
+
+renderPage('NBA');
