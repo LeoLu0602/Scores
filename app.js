e9d272679491e28a53036229869b7c9814bf8fdf
Leo <lyuholu@gmail.com>
2022-06-27 17:11:38 +0800

    setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
